60118ec7ead77a1ce14e3d175dfd2befe3be61c6
matheusruiz-07 <matheus.rgiatti@gmail.com>
2026-08-19 23:01:17 -0300

    feat: Tipagem de props

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,8 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Rodape from "./components/Rodape/rodape";
-import Saudacao from "./components/Saudacao/Saudacao";
+import Conteudo from "./components/Conteudo/Conteudo";
+
 
 export default function App() {
-
-  const nome:string = 'Matheus';
-  const sobrenome:string = 'Ruiz';
 
   return(
     <div>
@@ -13,14 +10,7 @@ export default function App() {
        * apresentação
        */}
       <Cabecalho/>
-      <main>
-        <div>
-          <Saudacao nome={"Matheus"}
-        </div>
-        <p>
-        <b>{`Nome:${nome} Sobrenome:${sobrenome}`}</b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae veniam reprehenderit necessitatibus, excepturi dolores veritatis iusto dignissimos dolor nobis eligendi, vitae expedita harum perspiciatis porro sapiente at odit explicabo consectetur.
-        </p>
-      </main>
+      <Conteudo
       <Rodape/>
     </div>
   );
